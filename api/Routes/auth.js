f574479859d896565279e90e042a3ff029bb7092
mahdialijafari <sedmahdialijafari@gmail.com>
2025-04-15 01:47:59 +0330

    completing auth and discount routes

--- api/Routes/auth.js
+++ api/Routes/auth.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import { auth, checkOtp, checkPassword, forgetPassword, resendCode } from '../Controllers/authCn.js'
 
 const authRouter=express.Router()
-
+authRouter.route('/').post(auth)
+authRouter.route('/otp').post(checkOtp)
+authRouter.route('/password').post(checkPassword)
+authRouter.route('/resend').post(resendCode)
+authRouter.route('/forget').post(forgetPassword)
 
 export default authRouter
--- api/Routes/disscountCode.js
+++ api/Routes/disscountCode.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import { isAdmin } from '../Middlewares/isAdmin'
+import { check, create, getAll, getOne, remove, update } from '../Controllers/disscountCodeCn'
+import { isLogin } from '../Middlewares/isLogin'
 
 const disscountCodeRouter=express.Router()
-
+disscountCodeRouter.route('/').post(isAdmin,create).get(isAdmin,getAll)
+disscountCodeRouter.route('/check').post(isLogin,check)
+disscountCodeRouter.route('/id').get(isAdmin,getOne).patch(isAdmin,update).delete(isAdmin,remove)
 
 export default disscountCodeRouter
